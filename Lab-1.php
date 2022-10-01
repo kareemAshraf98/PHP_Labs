dfb1500b3147cea9f97038ac255c4ca75e5b0498
kareemAshraf98 <karimhafez231@gmail.com>
2022-10-01 14:32:55 +0200

    Lab 1 & 2 PHP

---
+++ Lab-1.php
@@ -0,0 +1,82 @@
+<?php
+#1
+//echo phpinfo();
+
+
+#2
+// define("KAREEM","value of constant");
+// echo constant("KAREEM");
+
+
+#3
+// print_r($_SERVER["SERVER_NAME"]);
+// echo "<br>";
+// print_r($_SERVER["SERVER_ADDR"]);
+// echo "<br>";
+// print_r($_SERVER["SERVER_PORT"]);
+// echo "<br>";
+// print_r($_SERVER["SCRIPT_FILENAME"]);
+// echo "<br>";
+// print_r($_SERVER["DOCUMENT_ROOT"]);
+
+// $a = 0;
+// $b = 0;
+
+// for( $i = 0; $i<5; $i++ ) {
+//    $a += 10;
+//    echo $a;
+//    echo "<br>";
+//    $b += 5;
+//    echo $b;
+//    echo "<br>";
+// }
+
+// $i = 0;
+// $num = 50;
+
+// while( $i < 10) {
+//    $num--;
+//    echo $num;
+//    echo "<br>";
+//    $i++;
+//    echo $i;
+//    echo "<br>";
+// }
+
+// $i = 0;
+// $num = 0;
+         
+//  do {
+//     $i++;
+//     echo $i;
+//     echo "<br>";
+//     }
+ 
+// while( $i < 10 );
+// echo $i;
+
+         
+// $arr = array( 1, 2, 3, 4, 5);
+         
+// foreach( $arr as $value ) {
+//    echo "Value is $value <br />";
+// }
+
+
+
+#4
+// $age = 9;
+// switch($age){
+//     case($age < 5):
+//         echo "stay at home";
+//         break;
+//     case($age == 5):
+//         echo "Go to kindergarten";
+//         break;
+//     case($age >=6 && $age <= 12):
+//         echo " go to grade: ",$age-5;
+//         break;
+//     default:
+//     echo "wrong input";
+// }
+?>
---
+++ Lab-2.php
@@ -0,0 +1,69 @@
+<?php
+#1
+// echo nl2br("hello world \n we are programers \r\n in PHP");
+// echo "i am good";
+
+#2
+// echo nl2br(floor(1.60));
+// echo "<br>";
+// echo (abs(-90));
+// echo "<br>";
+// $str = "Hello world!";
+// echo chunk_split($str,1,".");
+
+#3
+// $sum = 0;
+// $avg = 0;
+// $arr = array(45,12,25,10);
+// rsort($arr);
+// $arrLength = count($arr);
+// for ($i =0; $i<$arrLength; $i++){
+//     $sum += $arr[$i];
+//     $avg = $sum /$arrLength;
+//     echo "<br>";
+//     echo $arr[$i];
+//     echo "<br>";
+// }
+// echo "sum = $sum   avg = $avg"; 
+
+#4
+// $newArr = array("Sara"=>31,"John"=>41,"Walaa"=>39,"Ramy"=>40);
+// asort($newArr);
+// ksort($newArr);
+// foreach ($newArr as $x=>$x_value){
+//     echo "Key =". $x .",Value =  ". $x_value;
+//     echo "<br>";
+// }
+// arsort($newArr);
+// krsort($newArr);
+// foreach ($newArr as $x=>$x_value){
+//     echo "Key =". $x .",Value =  ". $x_value;
+//     echo "<br>";
+// }
+
+#5
+// $students = [
+//     ['name' => 'Alaa', 'email' => 'alaa@test.com', 'status' => 'PHP'],
+//     ['name' => 'Shamy', 'email' => 'shamy@test.com', 'status' => '.Net'],
+//     ['name' => 'Youssef', 'email' => 'youssef@test.com', 'status' => 'Testing'],
+//     ['name' => 'Waleid', 'email' => 'waleid@test.com', 'status' => 'PHP'],
+//     ['name' => 'Rahma', 'email' => 'rahma@test.com', 'status' => 'Front End'],
+// ];
+// $arrLength = count($students);
+// echo "<center>";
+// echo "<table>";
+// echo "<tr><th>Name</th>     <th>E-mail</th>     <th>Status</th></tr>";
+// for($row = 0 ; $row<$arrLength; $row++){
+//     echo "<tr>";
+//     echo "<td>". $students[$row]['name']."</td>";
+//     echo "<td>". $students[$row]['email']."</td>";
+//     if($students[$row]['status'] == 'PHP'){
+//     echo '<td style= "color: red">'. $students[$row]['status'].'</td>';
+//     }else{
+//       echo '<td style>'. $students[$row]['status'].'</td>';
+//     }
+//     echo "</tr>";
+// }
+// echo "</table>";
+// echo "</center>";
+?>
